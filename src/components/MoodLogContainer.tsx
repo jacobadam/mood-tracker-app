eff569dcdae5325b819bd967996702c3e70d8794
jacobadam <jacobanevitt@gmail.com>
2025-01-27 16:01:45 -0800

    Add styling for mood log container, import mock data and pass as props to mood card

--- src/components/MoodLogContainer.tsx
+++ src/components/MoodLogContainer.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import MoodCard from "./MoodCard";
+import { moodsData } from "../moodData";
 
 const MoodLogContainer: React.FC = () => {
-  return <div>Mood Log Container</div>;
+  return (
+    <div className="flex flex-col gap-2 p-4 overflow-y-auto w-full no-scrollbar items-center">
+      {moodsData.map((mood) => (
+        <MoodCard
+          key={mood.id}
+          lottie={mood.lottie}
+          mood={mood.type}
+          date={mood.createdAt}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MoodLogContainer;
