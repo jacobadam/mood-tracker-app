68568a25083303d4b11257501618a73173d20e8b
jacobadam <jacobanevitt@gmail.com>
2025-01-30 15:52:26 -0800

    update styling for responsiveness

--- src/components/MoodTrackerApp.tsx
+++ src/components/MoodTrackerApp.tsx
@@ -5,12 +5,12 @@ import LogMoodButton from "./LogMoodButton";
 
 const MoodTrackerApp: React.FC = () => {
   return (
-    <div className="relative flex flex-col md:flex-row h-screen items-center justify-center p-12 box-border">
+    <div className="relative flex flex-col lg:flex-row h-screen items-center justify-center p-12 box-border lg:gap-2">
       <div className="flex flex-1 h-full max-w-full">
         <MoodAnimationContainer />
       </div>
 
-      <div className="relative flex max-w-full md:max-w-[30%] h-full flex-col items-center pr-32">
+      <div className="relative flex max-w-full lg:max-w-[30%] h-full flex-col items-center xl:pr-32">
         <MoodLogContainer />
         <LogMoodButton />
       </div>
