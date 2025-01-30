548e54f9980cc6abe83df8fa3c7d5e9b8f95101c
jacobadam <jacobanevitt@gmail.com>
2025-01-30 14:21:27 -0800

    add svg as placeholder

--- src/components/MoodAnimationContainer.tsx
+++ src/components/MoodAnimationContainer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const MoodAnimationContainer: React.FC = () => {
-  return <div>Mood Animation Container</div>;
+  return (
+    <div className="w-full h-full">
+      <img
+        src="/feeling-pleasant.svg"
+        alt="Feeling Pleasant"
+        className="w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default MoodAnimationContainer;
