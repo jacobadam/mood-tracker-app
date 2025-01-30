36cc312b63d23134c931009847c06b38c56ecb78
jacobadam <jacobanevitt@gmail.com>
2025-01-30 14:21:21 -0800

    add styling for app container positioning

--- src/components/MoodTrackerApp.tsx
+++ src/components/MoodTrackerApp.tsx
@@ -6,11 +6,11 @@ import LogMoodButton from "./LogMoodButton";
 const MoodTrackerApp: React.FC = () => {
   return (
     <div className="relative flex flex-col md:flex-row h-screen items-center justify-center p-12 box-border">
-      <div className="flex flex-1 max-w-full">
+      <div className="flex flex-1 h-full max-w-full">
         <MoodAnimationContainer />
       </div>
 
-      <div className="relative flex flex-1 max-w-full md:max-w-[25%] h-full flex-col items-center px-16">
+      <div className="relative flex max-w-full md:max-w-[30%] h-full flex-col items-center pr-32">
         <MoodLogContainer />
         <LogMoodButton />
       </div>
