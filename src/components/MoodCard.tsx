6cb33fa63fd59590ebcac361550fc2cbf6525eaf
jacobadam <jacobanevitt@gmail.com>
2025-01-30 15:52:45 -0800

    add slider for mood log on smaller screen

--- src/components/MoodCard.tsx
+++ src/components/MoodCard.tsx
@@ -42,37 +42,62 @@ const MoodCard: React.FC<MoodCardProps> = ({ lottie, mood, date }) => {
   const handleClick = () => {
     setIsSelected(!isSelected);
   };
-
   return (
-    <motion.button
-      onClick={handleClick}
-      whileHover={{
-        scale: 1.1,
-        backgroundColor: "var(--Lavender-200---Opacity, #C2C0FA80)",
-      }}
-      transition={{ duration: 0.2 }}
-      className={`flex items-center w-full justify-center px-10 h-20 rounded-3xl bg-white border-2 
+    <>
+      <motion.button
+        onClick={handleClick}
+        whileHover={{
+          scale: 1.1,
+          backgroundColor: "var(--Lavender-200---Opacity, #C2C0FA80)",
+        }}
+        transition={{ duration: 0.2 }}
+        className={`flex items-center w-full justify-center px-10 h-20 rounded-3xl bg-white border-2 
       focus:outline-none focus:ring-2 transition-transform 
-      ${isSelected ? "border-lavender-300" : "border-transparent"}`}
-    >
-      <div className="flex items-center justify-center gap-4">
-        <div className="w-16 h-12">
+      ${isSelected ? "border-lavender-300" : "border-transparent"}
+      hidden lg:block`}
+      >
+        <div className="flex items-center justify-center gap-4">
+          <div className="w-16 h-12">
+            <Lottie
+              options={defaultOptions}
+              isStopped
+              isPaused
+              className="w-full h-full"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center items-start w-full h-full">
+            <span className="text-lg font-medium text-gray-800">
+              {formattedMood}
+            </span>
+            <span className="text-sm text-gray-500">{finalFormattedDate}</span>
+          </div>
+        </div>
+      </motion.button>
+
+      <motion.button
+        onClick={handleClick}
+        whileHover={{
+          scale: 1.1,
+          backgroundColor: "var(--Lavender-200---Opacity, #C2C0FA80)",
+        }}
+        transition={{ duration: 0.2 }}
+        className="flex flex-col items-center justify-center w-40 h-40 bg-white p-6 rounded-3xl hover:bg-lavender-200 border-none text-2xl lg:hidden"
+      >
+        <div className="w-8 h-8 ">
           <Lottie
-            options={defaultOptions}
-            isStopped
-            isPaused
-            className="w-full h-full"
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: lottie,
+              rendererSettings: { preserveAspectRatio: "xMidYMid slice" },
+            }}
           />
         </div>
-
-        <div className="flex flex-col justify-center items-start w-full h-full">
-          <span className="text-lg font-medium text-gray-800">
-            {formattedMood}
-          </span>
-          <span className="text-sm text-gray-500">{finalFormattedDate}</span>
-        </div>
-      </div>
-    </motion.button>
+        <span className="mt-2 text-sm font-bold h-4">{formattedMood}</span>
+        <span className="mt-2 text-sm text-gray-500">{finalFormattedDate}</span>
+      </motion.button>
+    </>
   );
 };
 
--- src/components/MoodLogContainer.tsx
+++ src/components/MoodLogContainer.tsx
@@ -6,6 +6,11 @@ import { connectWebSocket, disconnectWebSocket } from "../utils/websocket";
 import pleasantLottie from "../assets/lottie/pleasant.json";
 import sadLottie from "../assets/lottie/sad.json";
 import excitedLottie from "../assets/lottie/excited.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 type MoodType = "PLEASANT" | "SAD" | "EXCITED";
 
@@ -62,23 +67,73 @@ const MoodLogContainer: React.FC = () => {
 
   return (
     <div className="fade-mask flex flex-col gap-2 p-4 overflow-y-auto w-full no-scrollbar items-center">
-      <AnimatePresence initial={false}>
-        {moods.map((mood) => (
-          <motion.div
-            key={mood.id}
-            layout
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-          >
-            <MoodCard
-              lottie={moodMap[mood.type]}
-              mood={mood.type}
-              date={mood.createdAt}
-            />
-          </motion.div>
-        ))}
-      </AnimatePresence>
+      <div className="hidden lg:flex flex-col gap-2 w-full">
+        <AnimatePresence initial={false}>
+          {moods.map((mood) => (
+            <motion.div
+              key={mood.id}
+              layout
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+            >
+              <MoodCard
+                lottie={moodMap[mood.type]}
+                mood={mood.type}
+                date={mood.createdAt}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
+
+      <div className="w-full p-8">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView="auto"
+          navigation={{
+            nextEl: ".portfolio-button-next",
+            prevEl: ".portfolio-button-prev",
+          }}
+          loop={true}
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 2,
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 2,
+            },
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 2,
+            },
+            748: {
+              slidesPerView: 4,
+              spaceBetween: 2,
+            },
+            900: {
+              slidesPerView: 5,
+              spaceBetween: 2,
+            },
+          }}
+        >
+          {moods.map((mood, index) => (
+            <SwiperSlide
+              key={index}
+              className="group"
+              aria-label={`Slide ${index + 1}`}
+            >
+              <MoodCard
+                lottie={moodMap[mood.type]}
+                mood={mood.type}
+                date={mood.createdAt}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
