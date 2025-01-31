e489ca380f034403470444bb29580d3734b496f9
jacobadam <jacobanevitt@gmail.com>
2025-01-30 16:15:11 -0800

    add responsiveness to modal

--- src/components/MoodSelectorModal.tsx
+++ src/components/MoodSelectorModal.tsx
@@ -30,7 +30,7 @@ const Modal: React.FC<ModalProps> = ({ onClose, onMoodSelect }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative p-8 rounded-2xl bg-lavender-100">
+      <div className="relative p-8 rounded-2xl bg-lavender-100 w-full sm:max-w-lg md:max-w-md lg:max-w-3xl xl:max-w-3xl sm:max-w-lg md:max-w-md lg:max-w-3xl xl:max-w-3xl">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 appearance-none border-none bg-transparent p-0"
@@ -39,7 +39,7 @@ const Modal: React.FC<ModalProps> = ({ onClose, onMoodSelect }) => {
           <img src={CloseIcon} alt="Close" className="w-6 h-6" />
         </button>
 
-        <h2 className="text-4xl font-extrabold mb-4 text-center pb-8">
+        <h2 className="text-lg lg:text-4xl font-extrabold mb-4 text-center pb-8">
           What mood are you in today?
         </h2>
 
@@ -47,10 +47,10 @@ const Modal: React.FC<ModalProps> = ({ onClose, onMoodSelect }) => {
           {moodButtons.map((mood, index) => (
             <button
               key={index}
-              className="flex flex-col items-center justify-center w-[252px] h-[242px] bg-white p-6 rounded-3xl hover:bg-lavender-200 border-none text-2xl"
+              className="flex flex-col items-center justify-center w-40 h-40 md:w-[252px] md:h-[242px] bg-white p-6 rounded-3xl hover:bg-lavender-200 border-none text-2xl"
               onClick={() => handleMoodClick(mood.type)}
             >
-              <div style={{ width: "64px", height: "64px" }}>
+              <div className="w-8 h-8 sm:w-16 sm:h-16">
                 <Lottie
                   options={{
                     loop: true,
