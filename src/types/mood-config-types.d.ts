45ada74e8ed4a4438f6a7824c5e218caa718e6d7
jacobadam <jacobanevitt@gmail.com>
2025-02-01 13:39:59 -0800

    add mood config types

---
+++ src/types/mood-config-types.d.ts
@@ -0,0 +1,36 @@
+export type LinearGradientStop = {
+  color: string;
+  offset?: number;
+  stopOpacity?: number;
+};
+
+export type LinearGradient = {
+  id: string;
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  stops: LinearGradientStop[];
+};
+
+export type MoodElement = {
+  id: string;
+  type: "ellipse" | "path";
+  cx?: number;
+  cy?: number;
+  rx?: number;
+  ry?: number;
+  d?: string;
+  fill: string;
+  initial?: Record<string, any>;
+  animate?: Record<string, any>;
+  exit?: Record<string, any>;
+  transition?: Record<string, any>;
+};
+
+export type MoodConfig = {
+  id: string;
+  backgroundGradientId: string;
+  elements: MoodElement[];
+  linearGradients: LinearGradient[];
+};
