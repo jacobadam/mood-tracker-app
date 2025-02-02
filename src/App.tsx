fcd31592a099369583eb14391b981935cca3bceb
jacobadam <jacobanevitt@gmail.com>
2025-02-02 08:23:03 -0800

    import react-router-dom and add routes for query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MoodTrackerApp from "./components/MoodTrackerApp";
 
 const App: React.FC = () => {
   return (
     <div className="bg-gray-100">
-      <MoodTrackerApp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MoodTrackerApp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
