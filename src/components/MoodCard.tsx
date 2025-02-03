63f46ad5db55a3ee96ca74b30d55ee77920c98c9
jacobadam <jacobanevitt@gmail.com>
2025-02-02 16:42:56 -0800

    remove motion and handle hover with state for controlling lottie and add styling

--- src/components/MoodCard.tsx
+++ src/components/MoodCard.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import Lottie from "react-lottie";
-import { motion } from "framer-motion";
 
 interface MoodCardProps {
   lottie: any;
   mood: "PLEASANT" | "SAD" | "EXCITED";
   date: string;
+  isSelected: boolean;
+  onMoodSelect: (mood: "PLEASANT" | "SAD" | "EXCITED") => void;
 }
 
-const MoodCard: React.FC<MoodCardProps> = ({ lottie, mood, date }) => {
-  const [isSelected, setIsSelected] = useState(false);
+const MoodCard: React.FC<MoodCardProps> = ({
+  lottie,
+  mood,
+  date,
+  isSelected,
+  onMoodSelect,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
 
   const formattedMood =
     mood.charAt(0).toUpperCase() + mood.slice(1).toLowerCase();
@@ -38,32 +45,23 @@ const MoodCard: React.FC<MoodCardProps> = ({ lottie, mood, date }) => {
     },
   };
 
-  // add functionality later
   const handleClick = () => {
-    setIsSelected(!isSelected);
+    onMoodSelect(mood);
   };
+
   return (
     <>
-      <motion.button
+      <button
         onClick={handleClick}
-        whileHover={{
-          scale: 1.1,
-          backgroundColor: "var(--Lavender-200---Opacity, #C2C0FA80)",
-        }}
-        transition={{ duration: 0.2 }}
-        className={`flex items-center w-full justify-center px-10 h-20 rounded-3xl bg-white border-2 
-      focus:outline-none focus:ring-2 transition-transform 
-      ${isSelected ? "border-lavender-300" : "border-transparent"}
-      hidden lg:block`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className={`hidden lg:flex items-center w-full min-w-80 justify-center px-6 h-20 rounded-3xl bg-white border-2 focus:outline-none focus:ring-2 transition-transform hover:h-32 hover:bg-lavender-200 lg:block ${
+          isSelected ? "border-lavender-300" : "border-transparent"
+        }`}
       >
         <div className="flex items-center justify-center gap-4">
           <div className="w-16 h-12">
-            <Lottie
-              options={defaultOptions}
-              isStopped
-              isPaused
-              className="w-full h-full"
-            />
+            <Lottie options={defaultOptions} isStopped={!isHovered} />
           </div>
 
           <div className="flex flex-col justify-center items-start w-full h-full">
@@ -73,30 +71,18 @@ const MoodCard: React.FC<MoodCardProps> = ({ lottie, mood, date }) => {
             <span className="text-sm text-gray-500">{finalFormattedDate}</span>
           </div>
         </div>
-      </motion.button>
+      </button>
 
-      <motion.button
+      <button
         onClick={handleClick}
-        whileHover={{
-          scale: 1.1,
-          backgroundColor: "var(--Lavender-200---Opacity, #C2C0FA80)",
-        }}
-        transition={{ duration: 0.2 }}
         className="flex flex-col items-center justify-center w-40 h-40 bg-white p-6 rounded-3xl hover:bg-lavender-200 border-none text-2xl lg:hidden"
       >
         <div className="w-8 h-8 ">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: lottie,
-              rendererSettings: { preserveAspectRatio: "xMidYMid slice" },
-            }}
-          />
+          <Lottie options={defaultOptions} isPaused />
         </div>
         <span className="mt-2 text-sm font-bold h-4">{formattedMood}</span>
         <span className="mt-2 text-sm text-gray-500">{finalFormattedDate}</span>
-      </motion.button>
+      </button>
     </>
   );
 };
