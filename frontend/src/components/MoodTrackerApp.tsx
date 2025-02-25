bc9fffa8e3a8d24135e5ad02ae058152351dd64d
jacobadam <jacobanevitt@gmail.com>
2025-02-25 10:13:29 -0800

    add padding to container

--- frontend/src/components/MoodTrackerApp.tsx
+++ frontend/src/components/MoodTrackerApp.tsx
@@ -28,7 +28,7 @@ const MoodTrackerApp: React.FC = () => {
         <MoodAnimationContainer mood={selectedMood} />
       </div>
 
-      <div className="relative flex max-w-full lg:max-w-[35%] h-full flex-col items-center">
+      <div className="relative flex max-w-full lg:max-w-[35%] h-full flex-col p-2">
         <MoodLogContainer
           selectedMood={selectedMood}
           onMoodSelect={handleMoodSelect}
