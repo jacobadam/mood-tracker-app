62b35fcc54e8660a7e8acd28ce932c121d831e06
jacobadam <jacobanevitt@gmail.com>
2025-02-28 13:25:27 -0800

    export mood text to separate file for cleanup

--- frontend/src/components/MoodAnimationContainer.tsx
+++ frontend/src/components/MoodAnimationContainer.tsx
@@ -11,43 +11,10 @@ import excitedLottie from "../../src/assets/lottie/excited.json";
 import { MoodConfig } from "../types/mood-config-types";
 import { MoodType } from "../types/mood-types";
 import { LottieData } from "../types/lottie-types";
+import { moodTexts } from "../data/mood-texts";
 
 type MoodProps = {
   mood?: MoodType;
-};
-
-const moodTexts = {
-  [MoodType.PLEASANT]: {
-    title: "You're feeling pleasant",
-    description: (
-      <>
-        Feeling on top of the world, are we? Must be
-        <br />
-        all those endorphins doing their happy dance!
-      </>
-    ),
-    color: "text-midnight-400",
-  },
-  [MoodType.SAD]: {
-    title: "You're feeling sad",
-    description: (
-      <>
-        Got the blues, huh? Remember, even clouds <br />
-        have silver linings. We’re here for you.
-      </>
-    ),
-    color: "text-white",
-  },
-  [MoodType.EXCITED]: {
-    title: "You're feeling excited",
-    description: (
-      <>
-        Buckle up, buttercup! Someone's got <br /> an extra sparkle in their
-        step today!
-      </>
-    ),
-    color: "text-white",
-  },
 };
 
 const moodConfigs: Record<MoodType, MoodConfig> = {
