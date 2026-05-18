bed23e30b075e8771944b289c39dc356a4f8c9b1
jacobadam <jacobanevitt@gmail.com>
2026-05-18 13:28:47 +0100

    remove dist from source control

--- backend/dist/types/mood-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MoodType = void 0;
-var MoodType;
-(function (MoodType) {
-    MoodType["SAD"] = "SAD";
-    MoodType["EXCITED"] = "EXCITED";
-    MoodType["PLEASANT"] = "PLEASANT";
-})(MoodType || (exports.MoodType = MoodType = {}));
