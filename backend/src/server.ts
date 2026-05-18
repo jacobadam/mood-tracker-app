4ad590b444a10c28231d500e8cb541d0942af116
jacobadam <jacobanevitt@gmail.com>
2026-05-18 13:40:46 +0100

    update localhost path

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
       ? process.env.FRONTEND_URL
-      : "http://localhost:3000",
+      : "http://localhost:5173",
   methods: ["GET", "POST", "DELETE"],
 };
 
